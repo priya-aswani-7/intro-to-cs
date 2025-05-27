f1825e0ae0feb507ac9e56fff732c3c068ea8cb5
priya-aswani-7 <priya.aswani28139@gmail.com>
2025-05-27 21:46:41 +0400

    add embedded for loop

--- printing.py
+++ printing.py
@@ -1,6 +1,8 @@
 print("hello world")
 print("goodbye")
 
+# list, dictionary, sets, tuples, classes & objects
+
 def printing(text):
     print(text)
     
@@ -10,6 +12,19 @@ printing("goodbye")
 # write algorithm to print numbers from 1 to 100
 # create a variable and assign it a 100
 # for loop to go from 1 to 100, and print at each step
-for i in range(1, 101):
-    print(i)
+# for i in range(1, 101):
+#     print(i)
+
+
+print("hi") # 1 unit of time
+text = "hello there" # 1 unit of time
+
+for i in range(1, 3): # n times
+    for j in range(1, 3): # n times
+        print(f"[{i}, {j}]")
+        
+# i = 1, j = 1
+# i = 1, j = 2
+# i = 2, j = 1
+# i = 2, j = 2
 
