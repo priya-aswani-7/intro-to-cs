8915b2f89f2006cdc17ed7b1019a266aca20672a
priya-aswani-7 <priya.aswani28139@gmail.com>
2025-05-27 21:21:51 +0400

    initial commit

---
+++ printing.py
@@ -0,0 +1,15 @@
+print("hello world")
+print("goodbye")
+
+def printing(text):
+    print(text)
+    
+printing("hello world")
+printing("goodbye")
+
+# write algorithm to print numbers from 1 to 100
+# create a variable and assign it a 100
+# for loop to go from 1 to 100, and print at each step
+for i in range(1, 101):
+    print(i)
+
